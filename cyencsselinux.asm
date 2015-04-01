9bb51a7bbe2c9da9a5ff294d5bc51a7ff37fb577
Anders Ponga <andersponga@gmail.com>
2015-04-01 18:10:48 +0200

    Adapted decode to linux version

--- cyencsselinux.asm
+++ cyencsselinux.asm
@@ -8,6 +8,10 @@ encode:
 	push r12 ; save r12 and r13, I need those registers for stuff
 	push r13
 	push rbx
+
+	sub rsp,24 ; 16 enough but want correct alignment
+	movdqu [rsp], xmm6
+
 	xor rbx, rbx
 	xor r9, r9
 	xor r11, r11
@@ -149,9 +153,140 @@ encode:
 
 .exitprogram:
 	mov rax, r9 ; Return output size
+	movdqu xmm6, [rsp]
+	add rsp,24
 	pop rbx
 	pop r13 ; restore some registers to their original state
 	pop r12
+	ret
+
+decode:
+	push rbx
+	movdqa xmm3, [specialdecode1]
+	movdqa xmm4, [specialdecode2]
+	movdqa xmm5, [specialdecode3]
+	xor rbx, rbx
+
+.decodeset:
+	cmp rdx, 16
+	jle .decspecialchar ; The last 8 or less characters need special treatment
+	pxor xmm1, xmm1
+	movdqu xmm0, [rdi] ; Read from memory
+
+	movdqa xmm2, xmm0 ; temporary copy
+	pcmpeqb xmm2, xmm3 ; Check for special chars
+	por xmm1, xmm2
+	movdqa xmm2, xmm0
+	pcmpeqb xmm2, xmm4
+	por xmm1, xmm2
+	movdqa xmm2, xmm0
+	pcmpeqb xmm2, xmm5
+	por xmm1, xmm2
+	movq r10, xmm1
+	cmp r10, 0
+	jne .decodemultientry ; Let's do special stuff!
+	psrldq xmm1, 8
+	movq r10, xmm1
+	cmp r10, 0
+	jne .decodemultientry ; Let's do special stuff!
+	cmp bl, 1 ; Check if there was a = last in last batch
+	je .decodemultientry
+	psubb xmm0, [const1] ; - 42
+	movdqu [rsi], xmm0 ; Move decoded byte to output array
+	add rsi, 16 ; increase output array pointer
+	add rdi, 16 ; increase input pointer
+	add rcx, 16 ; Increase size of output
+	sub rdx, 16 ; Done encoding 16 bytes
+	jmp .decodeset ; Encode another 16 bytes
+
+.decodemultientry:
+	xor r11, r11
+	movq rax, xmm0 ; Move to gpr so we can do stuff
+	cmp rax, 0
+	jz .decodenextset ; If it's zero it means we've already done all the bytes in xmm0
+	psrldq xmm0, 8 ; Shift bytes
+
+.decodemulti:
+	add r11, 1
+	cmp bl, 1
+	je .decfixbreak
+	cmp al, 61
+	je .decodespecial
+	cmp al, 10
+	je .decskipbyte
+	cmp al, 13
+	je .decskipbyte
+
+.decodenextcharmulti:
+	sub al, 42 ; Decode
+	mov byte [rsi], al ; Move decoded byte to output array
+	add rsi, 1 ; increase output array pointer
+	add rcx, 1 ; Increase size of output
+	sub rdx, 1
+	jz .decodeexitprogram
+	cmp r11, 8
+	je .decodemultientry
+	ror rax, 8
+	jmp .decodemulti
+
+.decskipbyte:
+	sub rdx, 1
+	jz .decodeexitprogram
+	cmp r11, 8
+	je .decodemultientry
+	ror rax, 8
+	jmp .decodemulti
+
+.decodespecial:
+	sub rdx, 1
+	jz .decodeexitprogram
+	add bl, 1
+	cmp r11, 8
+	je .decodemultientry
+	ror rax, 8
+	add r11, 1
+
+.decfixbreak:
+	sub al, 64 ; Decode
+	sub bl, 1
+	jmp .decodenextcharmulti
+
+.decodenextset:
+	add rdi, 16
+	jmp .decodeset
+
+.decscnextchar:
+	add rdi, 1
+	sub rdx, 1
+	jz .decodeexitprogram
+
+.decspecialchar:
+	mov r10b, byte [rdi] ; Move character from memory to register
+	cmp r10b, 61
+	je .decsc
+	cmp r10b, 10
+	je .decscnextchar
+	cmp r10b, 13
+	je .decscnextchar
+	jmp .decscoutputencoded
+
+.decsc:
+	sub rdx, 1
+	jz .decodeexitprogram
+	add rdi, 1
+	mov r10b, byte [rdi] ; Move character from memory to register
+	sub r10b, 64 ; This time we sub 64
+
+.decscoutputencoded:
+	sub r10b, 42 ; Sub 42 before modulus
+	mov byte [rsi], r10b ; Move encoded byte to output array
+	add rsi, 1 ; increase output array pointer
+	add rcx, 1 ; Increase size of output
+	jmp .decscnextchar
+
+.decodeexitprogram:
+	mov rax, rcx ; Return output size
+	pop rbx
 	ret
 
 section .data
